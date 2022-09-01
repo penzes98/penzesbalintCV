f310a1543ab96568b417ba1439aa4178c8566245
Pénzes Bálint <penzes98@gmail.com>
2022-09-01 21:31:15 +0200

    sidebar

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import SchoolIcon from '@mui/icons-material/School';
+import InfoIcon from '@mui/icons-material/Info';
+import WorkIcon from '@mui/icons-material/Work';
+export const SidebarData = [
+    {
+        title:"Home",
+        icon: <HomeIcon />,
+        path: "/home"
+    },
+    {
+        title:"Schools",
+        icon: <SchoolIcon />,
+        path: "/schools"
+    },
+    {
+        title:"Experiences",
+        icon: <WorkIcon />,
+        path: "/experiences"
+    },
+    {
+        title:"About Me",
+        icon: <InfoIcon />,
+        path: "/aboutMe"
+    },
+    
+]
+
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from "../Components/Sidebar";
+import '../App.css';
+
+function Home(){
+    return (
+        <div className="Home">
+            <Sidebar />
+            <h1>Welcome to my CV webpage!</h1>
+        </div>
+      );
+      
+}
+
+export default Home
